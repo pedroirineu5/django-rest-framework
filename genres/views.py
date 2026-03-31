8b186270900ce7ecd77219d054cf52e35a0ce83b
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-03-31 10:41:47 -0300

    feat(genres): post and get adding

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,36 @@
+import json
+
 from django.http import JsonResponse
 
 from genres.models import Genre
 
 
+## fazendo no braço para entender tudo
 def genre_view(request):
-    genres = Genre.objects.all()
-    return JsonResponse(genres)
+
+    if request.method == 'GET':
+            
+        genres = Genre.objects.all()
+
+        data = []
+        for genre in genres:
+            data.append(
+                {
+                    'id': genre.id,
+                    'name': genre.name
+                }
+            )
+        return JsonResponse(data, safe=False)
+    
+    if request.method == 'POST':
+        data_user = json.load(request.body.decode('utf-8'))
+        new_genre = Genre(name=data_user['name'])
+        new_genre.save()
+        return JsonResponse(
+            {
+                'id': new_genre.id,
+                'name': new_genre.name
+            },
+            status = 201,
+        )
+
