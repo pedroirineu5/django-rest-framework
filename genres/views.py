bd9821ea02acfb7441c77c2a1b7a465df2625825
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-01 15:23:46 -0300

    feat: put method

--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
 from genres.models import Genre
@@ -40,10 +41,23 @@ def genre_view(request):
 
 @csrf_exempt
 def detail_view_genre(request,pk):
-    ## OPERAÇÃO COM O ORM
-    genre = Genre.objects.get(pk=pk)
+    
+    # OPERAÇÃO COM O ORM
+    genre = get_object_or_404(Genre, pk=pk)
 
-
-    data = {'id': genre.id,'name':genre.name}
-    return JsonResponse(data)
-
+    if request.method == 'GET':
+        data = {'id': genre.id,'name':genre.name}
+        return JsonResponse(data)
+    
+    elif request.method == 'PUT':
+        # convertendo os usuarios a usarem o melhor 
+        data_user_put = json.loads(request.body.decode('utf-8'))
+        genre.name = data_user_put['name']
+        genre.save()
+        
+        return JsonResponse(
+            {
+                'id': genre.id,
+                'name': genre.name
+            }
+        ) 
