5e0ebd718d34ffa95782cd7b986316d23313ca47
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-04 00:08:09 -0300

    feat: adding the ClassbasedView and serializers to the API

--- genres/views.py
+++ genres/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 
 from genres.models import Genre
+from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
@@ -15,41 +16,45 @@ class GenreCreateListView(generics.ListCreateAPIView):
     # vai transformar os objetos em python para JSON como request e response
     # Eles funcionam na serialização: Que transforma o objeto python em JSON (SAIDA DE DADOS NA APLICAÇÃO)
     # e tbm na DESSERIALIZAÇÃO: que transforma do json para um objeto python em assim usar nas validações do backend. (Entrada de dados na APLICAÇÃO)
-    serializer_class = None
+    serializer_class = GenreSerializer
 
 
 
-# fazendo no braço para entender tudo
-@csrf_exempt
-def genre_view(request):
 
-    if request.method == 'GET':
+###############################
+#       sem usar o drf        #
+###############################
+# # fazendo no braço para entender tudo
+# @csrf_exempt
+# def genre_view(request):
+
+#     if request.method == 'GET':
             
-        genres = Genre.objects.all()
+#         genres = Genre.objects.all()
 
-        data = []
-        for genre in genres:
-            data.append(
-                {
-                    'id': genre.id,
-                    'name': genre.name
-                }
-            )
-        return JsonResponse(data, safe=False)
+#         data = []
+#         for genre in genres:
+#             data.append(
+#                 {
+#                     'id': genre.id,
+#                     'name': genre.name
+#                 }
+#             )
+#         return JsonResponse(data, safe=False)
     
-    if request.method == 'POST':
+#     if request.method == 'POST':
 
-        data_user = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data_user['name'])
-        new_genre.save()
+#         data_user = json.loads(request.body.decode('utf-8'))
+#         new_genre = Genre(name=data_user['name'])
+#         new_genre.save()
         
-        return JsonResponse(
-            {
-                'id': new_genre.id,
-                'name': new_genre.name
-            },
-            status = 201,
-        )
+#         return JsonResponse(
+#             {
+#                 'id': new_genre.id,
+#                 'name': new_genre.name
+#             },
+#             status = 201,
+#         )
 
 @csrf_exempt
 def detail_view_genre(request,pk):
