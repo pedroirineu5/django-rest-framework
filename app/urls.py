a36892f91f3c0c1f3315f3c461a41f803ea9e30c
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-04 00:13:55 -0300

    feat: urls changes to CBV

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
 
-from genres.views import detail_view_genre, genre_view
+from genres.views import GenreCreateListView, detail_view_genre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-view'),
+    path('genres/',GenreCreateListView.as_view() , name='genre-view'),
     path('genres/<int:pk>/', detail_view_genre, name='detail-view-genre')
 ]
     
