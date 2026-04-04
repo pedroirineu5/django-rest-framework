5b19b77851c28949820e69c609652ceacbb2626e
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-03 23:31:37 -0300

    feat: generics for DRF

--- genres/views.py
+++ genres/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import genericss
 
 from genres.models import Genre
 
