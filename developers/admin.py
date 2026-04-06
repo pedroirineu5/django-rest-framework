ae2d5c66a80b71f8db9fdb7c8db06f1ec4891250
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-06 17:20:08 -0300

    feat: app

---
+++ developers/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from developers.models import Developers
+
+
+@admin.register(Developers)
+class AdminDevelopers(admin.ModelAdmin):
+    list_display = ('id','name')
