753294eeb8ff5c383eca434ace0382ae88d69184
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-06 17:11:29 -0300

    feat: adds class based views to the urls

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
 
+from developers.views import (DevelopersCreateClass,
+                              DevelopersDetailUpdateDestroyClass)
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/',GenreCreateListView.as_view() , name='genre-view'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='detail-view-genre')
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='detail-view-genre'),
+    path('developers/', DevelopersCreateClass.as_view(), name='developers-view'),
+    path('developers/<int:pk>/', DevelopersDetailUpdateDestroyClass.as_view(),name='Detail_view')
 ]
     
