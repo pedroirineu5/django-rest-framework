6b2929107ea2953fc218132b7584ec10c2265860
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-06 17:10:37 -0300

    feat: model for developers

---
+++ developers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+NATIONALITY_CHOICES = (
+    ('USA', 'AMERICAN'),
+    ('BRAZIL', 'BRAZILLIAN'),
+    ('JAPAN', 'JAPANESE')
+    )
+
+
+class Developers(models.Model):
+
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField(max_length=100, choices=NATIONALITY_CHOICES, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
