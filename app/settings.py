9278021b7883360f3a02acbd3682db59947d9181
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-06 17:10:46 -0300

    feat; adds the app to the core

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'genres',
-    'rest_framework'
+    'rest_framework',
+    'developers'
 ]
 
 MIDDLEWARE = [
