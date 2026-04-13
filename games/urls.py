635d4746638b496643100960988d55b942118b35
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-13 15:34:58 -0300

    feat: add URL patterns for games list and detail views

---
+++ games/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('games/', views.GamesListCreateView.as_view(), name='games_list'),
+    path('games/<int:pk>/', views.GamesRetrieveUpdateDestroyAPIView.as_view(), name="games_detail"),
+  
+]
