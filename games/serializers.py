9c894bbba76096deac53645fcaa46435486273c3
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-17 15:08:08 -0300

    feat: implement rate calculation in Games serializer

--- games/serializers.py
+++ games/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from games.models import Games
@@ -7,16 +8,21 @@ class GamesSerializers(serializers.ModelSerializer):
 
 
     # Serlializer method field, isso é um campo que reconhece que ele vai ter uma função dentro da classe para calcula-lo
-    rate = serializers.SerializerMethodField(read_only=True)
     # apenas sendo usado como um campo de GET, ou seja, leitura.
-
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Games
         fields = "__all__"
 
     def get_rate(self, obj):
-        ...
+        reviews = obj.reviews.aggregate(Avg('grade'))['grade__avg']
+        
+        if reviews:
+            return round(reviews, 1)
+
+        return None
+
     
     def validate_release_date(self, value):
         if value.year < 1962:
