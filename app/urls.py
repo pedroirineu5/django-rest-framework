acfccb5ed6afd5e2f112a339605ce2cf65ed0cbb
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-30 18:24:53 -0300

    feat: authentication app working with proper urls setted]

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -18,9 +14,8 @@ urlpatterns = [
     
     path('api/v1/', include('games.urls')),
 
-    path('api/v1/', include('reviews.urls'))
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('')
+    path('api/v1/', include('reviews.urls')),
+    
+    path('api/v1/', include('authentication.urls'))
 ]
     
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
+
+urlpatterns=[
+    path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+]
