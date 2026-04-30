7ce624a61c8b569448e631c0c8c2ca85d5d35094
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-04-30 18:08:25 -0300

    feat: add JWT authentication support with token endpoints

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -15,5 +19,8 @@ urlpatterns = [
     path('api/v1/', include('games.urls')),
 
     path('api/v1/', include('reviews.urls'))
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('')
 ]
     
