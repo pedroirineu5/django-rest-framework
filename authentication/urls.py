6c5d29357b5d5c5f71884ba80d3421b7150797b2
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-05-02 22:02:17 -0300

    feat: integrate JWT authentication with REST framework and update URL patterns

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import AuthenticationView
 
+## TODO  continuar com auth jwt
 
+urlpatterns = [
+    path('authentication/token/',AuthenticationView.as_view(), name='authentication'),
+]
