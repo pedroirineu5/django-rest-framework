6e2007b24870cfd239555fd628d0a9aab54f57cf
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-05-03 23:58:02 -0300

    refact: remove unused JWT token views from URL patterns

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
